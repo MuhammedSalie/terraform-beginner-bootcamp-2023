412c73f4318c1bb3d989a7b42dbe4896b78ae989
MuhammedSalie <muhammed.salie@mmltd.co.za>
2023-10-03 15:45:29 +0000

    create s3 bucket

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
+#terraform {
+#  required_providers {
+#    random = {
+#      source = "hashicorp/random"
+#      version = "3.5.1"
+#    }
+#  }
+#}
+
 terraform {
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
     }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -14,8 +27,19 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  lower            = true
+  upper            = false
 }
 
 output random_bucket_name {
     value = random_string.bucket_name.id
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.id
+
+  tags = {
+    Name        = random_string.bucket_name.id
+    Environment = "Dev"
+  }
 }
