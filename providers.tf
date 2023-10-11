02981f47afec8e4e6e781904b1b4cb1188432586
MuhammedSalie <muhammed.salie@mmltd.co.za>
2023-10-11 11:34:25 +0000

    change structure of root module

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+#terraform {
+#  cloud {
+#    organization = "MMIHoldings_Platforms_Test"
+#
+#    workspaces {
+#      name = "Terra-house-1"
+#    }
+#  }
+#}
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
