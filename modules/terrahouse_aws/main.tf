ee936f2441afd4216d81fee5fe09af77ea5f4b1f
MuhammedSalie <130761042+MuhammedSalie@users.noreply.github.com>
2023-10-12 17:57:20 +0200

    Static website (#32)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -17,3 +17,29 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = "/workspace/terraform-beginner-bootcamp-2023/public/index.html"
+  etag = filemd5("/workspace/terraform-beginner-bootcamp-2023/public/index.html")
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = "/workspace/terraform-beginner-bootcamp-2023/public/error.html"
+  etag = filemd5("/workspace/terraform-beginner-bootcamp-2023/public/error.html")
+}
